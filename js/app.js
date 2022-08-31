a5d0b99420f258605a8c57b0b5318c270ca8aec6
resalatislam5 <resalatislam5@gmail.com>
2022-09-01 00:39:50 +0600

    sideber

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
 const loadAllPouducts = async() =>{
     const res = await  fetch('https://fakestoreapi.com/products');
     const data = await  res.json();
-    console.log(data)
+    return data;
+}
+const displaySideBer = async() =>{
+    const loadProducts = await loadAllPouducts();
+    const findSideBerName = [];
+const sideBer = document.getElementById('side-ber');
+    for (const product of loadProducts) {
+        const {category} = product
+        if(findSideBerName.indexOf(category) == -1){
+            findSideBerName.push(category)
+            const li = document.createElement('li');
+            li.innerHTML =`
+            <li class ="text-2xl font-semiboldbold py-4">${category}</li>
+            `
+            sideBer.appendChild(li)
+        }
+    }
 }
 
-loadAllPouducts()
+
+displaySideBer()
